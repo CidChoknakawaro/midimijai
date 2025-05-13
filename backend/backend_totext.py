602e02f3035c3c38f8b0ac7284b2ddb3d373fccb
Cid Choknakawaro <Cid.Choky@gmail.com>
2025-05-13 16:26:48 +0700

    Renamed code to text files

--- backend/backend_totext.py
+++ backend/backend_totext.py
@@ -1,11 +1,11 @@
 from codebase_to_text import CodebaseToText
 
 code_to_text = CodebaseToText(
-    input_path='frontend\src',
-    output_path="frontend/code_output.txt",             # where to save the result
+    input_path='backend/app',
+    output_path="backend/backend_code.txt",             # where to save the result
     output_type="txt",                  # output format
     verbose=True,                       # show logging output
-    exclude_hidden=True                 # skip hidden files like .git, .env, etc.
+    exclude_hidden=True,                 # skip hidden files like .git, .env, etc.
 )
 
 code_to_text.get_file()
---
+++ frontend/frontend_totext.py
@@ -0,0 +1,11 @@
+from codebase_to_text import CodebaseToText
+
+code_to_text = CodebaseToText(
+    input_path='frontend/src',
+    output_path="frontend/frontend_code.txt",             # where to save the result
+    output_type="txt",                  # output format
+    verbose=True,                       # show logging output
+    exclude_hidden=True,                 # skip hidden files like .git, .env, etc.
+)
+
+code_to_text.get_file()
